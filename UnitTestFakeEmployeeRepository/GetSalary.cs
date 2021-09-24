d452f469517189c84d48a35649c11390c2878266
Shawn Ensz <Shawn.Ensz@trojans.dsu.edu>
2021-09-23 19:47:33 -0500

    Changed project to a forms project

---
+++ UnitTestFakeEmployeeRepository/GetSalary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFakeEmployeeRepository
+{
+    [TestClass]
+    public class GetSalary
+    {
+        [TestMethod]
+        public void GetSalaryWithGoodIdIsCorrect()
+        {
+            //Arrange
+            FakeEmployeeRepository EmpRepositor = new FakeEmployeeRepository();
+            //Act
+
+            //Assert
+        }
+    }
+}
